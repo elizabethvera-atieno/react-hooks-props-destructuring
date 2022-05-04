d230d4314be75bc226a9a2f7af4c9e0d03c835b3
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-04 13:08:39 +0300

    code along

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// function SocialMedia({socialLinks}){
+//     return (
+//         <div>
+//           <a href={socialLinks.github}>{socialLinks.github}</a>
+//           <a href={socialLinks.linkedin}>{socialLinks.linkedin}</a>
+//         </div>
+//       );
+// }
+function SocialMedia({socialLinks :{ github, linkedin }}){
+    return (
+        <div>
+        <a href={github}>{github}</a>
+        <a href={linkedin}>{linkedin}</a> 
+        </div>
+    );
+}
+
+
+
+
+export default SocialMedia
